ad75376439af67d425c4e494ae9377f4115dd5af
epiglotis <83986620+epiglotis@users.noreply.github.com>
2023-12-14 05:41:56 +0300

    Added Logo to Twitter Download Page

--- src/app/twitter/page.tsx
+++ src/app/twitter/page.tsx
@@ -2,10 +2,13 @@
 
 import dynamic from 'next/dynamic';
 import TwitterComponent from './TwitterComponent';
+import Image from 'next/image';
+import TwitterLogo from "../../../public/twitterXLogo.svg"
 
 const Twitter: React.FC = () => {
   return (
     <main className='flex min-h-screen flex-col items-center justify-center p-24'>
+      <Image src={TwitterLogo} alt='TwitterLogo' width={100} height={24}/>
       <h1 className='p-5 text-2xl text-center'>
         Twitter / X Video Downloader
       </h1>
