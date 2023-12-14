37182691a307a25e17dafc70eeb38ccc1f9994e5
epiglotis <83986620+epiglotis@users.noreply.github.com>
2023-12-14 05:45:05 +0300

    Youtube Page Integrated

---
+++ src/app/youtube/page.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+// import dynamic from 'next/dynamic';
+// import TwitterComponent from './TwitterComponent';
+import Image from 'next/image';
+import YoutubeLogo from "../../../public/youtubeLogo.svg"
+
+const Twitter: React.FC = () => {
+  return (
+    <main className='flex min-h-screen flex-col items-center justify-center p-24'>
+      <Image src={YoutubeLogo} alt='TwitterLogo' width={100} height={24}/>
+      <h1 className='p-5 text-2xl text-center'>
+        Twitter / X Video Downloader
+      </h1>
+      {/* <TwitterComponent /> */}
+    </main>
+  );
+};
+
+export default Twitter;
